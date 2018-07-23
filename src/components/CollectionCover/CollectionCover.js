ed17015c4b33e5f92d850fc4ea311565436d9293
DJLionheart <aundario@gmail.com>
2018-07-23 16:59:39 -0600

    component creation

---
+++ src/components/CollectionCover/CollectionCover.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CollectionCover() {
+    return(
+        <div>
+            <h1>CollectionCover</h1>
+        </div>
+    )
+}
+
+export default CollectionCover;
---
+++ src/components/LinkList/LinkList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LinkList() {
+    return(
+        <div>
+            <h1>LinkList</h1>
+        </div>
+    )
+}
+
+export default LinkList;
