dbfb5ab687b3589c073d7c321184cb34557409b2
DJLionheart <aundario@gmail.com>
2018-08-01 15:35:17 -0600

    additional components and media assets

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import Cart from './components/Cart/Cart';
